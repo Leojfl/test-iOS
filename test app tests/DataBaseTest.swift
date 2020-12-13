7216a69aa77c1e1baf0b069af8a85242d01acc0c
Leonardo Flores <leojfl@outlook.com>
2020-12-13 12:15:17 -0600

    init with unit test

---
+++ test app tests/DataBaseTest.swift
@@ -0,0 +1,26 @@
+//
+//  DataBaseTest.swift
+//  test app tests
+//
+//  Created by Leonardo Flores Lopez on 13/12/20.
+//
+
+import XCTest
+
+class DataBaseTest: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testSaveTVShow() throws {
+    }
+    
+    
+    func testDeleteTVShow() throws {
+    }
+
+}
